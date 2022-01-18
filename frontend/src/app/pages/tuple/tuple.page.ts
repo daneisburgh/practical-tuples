cb8c84a67a6bed8f8fdd293abdd10b12d76eaf24
Dane Isburgh <daneisburgh@gmail.com>
2022-01-18 17:48:40 -0500

    feat: add initial tuple page functionality

--- frontend/src/app/pages/tuple/tuple.page.ts
+++ frontend/src/app/pages/tuple/tuple.page.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { toInteger } from "lodash";
 
 import { Tuple } from "../../services/resources/tuples/tuples.service";
 import { UsersService } from "../../services/resources/users/users.service";
+import { TupleItemsService } from "../../services/resources/tuple-items/tuple-items.service";
 
 @Component({
     selector: "app-tuple",
@@ -11,9 +12,15 @@ import { UsersService } from "../../services/resources/users/users.service";
     styleUrls: ["./tuple.page.scss"]
 })
 export class TuplePage implements OnInit {
+    creatingTupleItem = false;
     tuple: Tuple;
 
-    constructor(private activatedRoute: ActivatedRoute, private usersService: UsersService) {}
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private router: Router,
+        private usersService: UsersService,
+        private tupleItemsService: TupleItemsService
+    ) {}
 
     get user() {
         return this.usersService.user;
@@ -21,9 +28,26 @@ export class TuplePage implements OnInit {
 
     ngOnInit() {
         const id = toInteger(this.activatedRoute.snapshot.paramMap.get("id"));
+        this.setTuple(id);
+    }
 
+    async createTupleItem() {
+        this.creatingTupleItem = true;
+        const { id } = await this.tupleItemsService.create();
+        this.setTuple(id);
+        this.creatingTupleItem = false;
+    }
+
+    private setTuple(id: number) {
         if (this.user) {
-            this.tuple = this.user.tuples.find((tuple) => tuple.id === id);
+            const tuple = this.user.tuples.find((t) => t.id === id);
+
+            if (tuple) {
+                this.tuple = tuple;
+                console.log(tuple);
+            } else {
+                this.router.navigateByUrl("/error", { skipLocationChange: true });
+            }
         }
     }
 }
