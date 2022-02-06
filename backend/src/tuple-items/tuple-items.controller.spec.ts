86b81e6a66f455547772a6ce9142568fe7982699
Dane Isburgh <daneisburgh@gmail.com>
2022-02-06 16:35:22 -0500

    test: fix tuple items tests

--- backend/src/tuple-items/tuple-items.controller.spec.ts
+++ backend/src/tuple-items/tuple-items.controller.spec.ts
@@ -1,6 +1,16 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+
+import { TupleItem } from "./entities/tuple-item.entity";
 import { TupleItemsController } from "./tuple-items.controller";
 import { TupleItemsService } from "./tuple-items.service";
+import { TuplesService } from "../tuples/tuples.service";
+import { Tuple } from "../tuples/entities/tuple.entity";
+import { UsersService } from "../users/users.service";
+import { User } from "../users/entities/user.entity";
+import { UserGuard } from "../users/guards/user.guard";
+import { UserConnectionGuard } from "../users/guards/user-connection.guard";
 
 describe("TupleItemsController", () => {
     let controller: TupleItemsController;
@@ -8,7 +18,25 @@ describe("TupleItemsController", () => {
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [TupleItemsController],
-            providers: [TupleItemsService]
+            providers: [
+                TupleItemsService,
+                TuplesService,
+                UsersService,
+                UserGuard,
+                UserConnectionGuard,
+                {
+                    provide: getRepositoryToken(TupleItem),
+                    useClass: Repository
+                },
+                {
+                    provide: getRepositoryToken(Tuple),
+                    useClass: Repository
+                },
+                {
+                    provide: getRepositoryToken(User),
+                    useClass: Repository
+                }
+            ]
         }).compile();
 
         controller = module.get<TupleItemsController>(TupleItemsController);
--- backend/src/tuple-items/tuple-items.service.spec.ts
+++ backend/src/tuple-items/tuple-items.service.spec.ts
@@ -1,4 +1,8 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+
+import { TupleItem } from "./entities/tuple-item.entity";
 import { TupleItemsService } from "./tuple-items.service";
 
 describe("TupleItemsService", () => {
@@ -6,7 +10,13 @@ describe("TupleItemsService", () => {
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [TupleItemsService]
+            providers: [
+                TupleItemsService,
+                {
+                    provide: getRepositoryToken(TupleItem),
+                    useClass: Repository
+                }
+            ]
         }).compile();
 
         service = module.get<TupleItemsService>(TupleItemsService);
