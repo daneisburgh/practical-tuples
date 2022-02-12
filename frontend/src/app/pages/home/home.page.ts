e8712648ca77c83bb4939f62b390cc8e8e2e73c3
Dane Isburgh <daneisburgh@gmail.com>
2022-02-12 17:46:48 -0500

    feat: update home tuple content and global styles

--- frontend/src/app/pages/home/home.page.ts
+++ frontend/src/app/pages/home/home.page.ts
@@ -1,7 +1,8 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
-import { TuplesService } from "src/app/services/resources/tuples/tuples.service";
+import { AlertController } from "@ionic/angular";
 
+import { Tuple, TuplesService, TupleType } from "../../services/resources/tuples/tuples.service";
 import { UsersService } from "../../services/resources/users/users.service";
 
 @Component({
@@ -10,9 +11,11 @@ import { UsersService } from "../../services/resources/users/users.service";
     styleUrls: ["home.page.scss"]
 })
 export class HomePage {
-    creatingTuple = false;
+    deletingTupleId = -1;
+    isCreatingTuple = false;
 
     constructor(
+        private alertController: AlertController,
         private router: Router,
         private tuplesService: TuplesService,
         private usersService: UsersService
@@ -22,10 +25,51 @@ export class HomePage {
         return this.usersService.user;
     }
 
+    get isInputDisabled() {
+        return this.deletingTupleId > -1 || this.isCreatingTuple;
+    }
+
     async createTuple() {
-        this.creatingTuple = true;
+        this.isCreatingTuple = true;
         const { id } = await this.tuplesService.create();
         this.router.navigateByUrl("/tuple/" + id);
-        this.creatingTuple = false;
+        this.isCreatingTuple = false;
+    }
+
+    async deleteTuple(tuple: Tuple) {
+        const alert = await this.alertController.create({
+            message: "Please confirm tuple deletion. This action is irreversible.",
+            buttons: [
+                {
+                    text: "Delete",
+                    cssClass: "alert-button-danger",
+                    handler: async () => {
+                        this.deletingTupleId = tuple.id;
+                        await this.tuplesService.delete(tuple.id);
+                        this.router.navigateByUrl("/");
+                        this.deletingTupleId = -1;
+                    }
+                },
+                {
+                    text: "Cancel",
+                    role: "cancel"
+                }
+            ]
+        });
+
+        await alert.present();
+    }
+
+    getTupleItemsString(tuple: Tuple) {
+        const { tupleItems, type } = tuple;
+        const tupleItemsLengthString = tupleItems.length.toString();
+        let tupleItemsString = tupleItemsLengthString;
+
+        if (tupleItems.length > 0 && type === TupleType.checkbox) {
+            const checked = tupleItems.filter((tupleItem) => tupleItem.isChecked).length;
+            tupleItemsString += ` (${checked}/${tupleItemsLengthString} checked)`;
+        }
+
+        return tupleItemsString;
     }
 }
